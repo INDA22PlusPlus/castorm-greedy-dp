53355ae1bab8ba88b390cc23eca6dfaa3c7e3928
castormann <castormannen@gmail.com>
2023-01-26 11:09:15 +0100

    bank solution shortened in order to place high in statistics :D

--- bank.py
+++ bank.py
@@ -1,21 +1,7 @@
-N, T = map(int, input().split())
-people = []
-for i in range(N):
-    c, t = map(int, input().split())
-    people.append((c,t))
-    
-t = 0
-
-people.sort(key=lambda x: -(x[0] * 48 - x[1]))
-res = [None for _ in range(T + 1)]
-while people and t <= T:
-    curr = people.pop(0)
-    for i in range(curr[1] + 1):
-        if res[curr[1] - i] == None:
-            res[curr[1] - i] = curr[0]
-            t += 1
-            break
-tot = 0
-for i in res:
-    if i != None: tot += i
-print(tot)
+def f():return tuple(map(int,input().split()))
+N,T=f();p,t,s,x=sorted([f() for _ in range(N)]),0,0,[]
+while p and t<=T:
+ c,d=p.pop()
+ for i in range(d+1):
+  if d-i not in x:x.append(d-i);s+=c;t+=1;break
+print(s)
